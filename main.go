27a3def482dcd595449608eb6700a434077bf465
dewble <dewble62@gmail.com>
2021-09-06 22:59:31 +0900

    add procfile

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"dewble/todos/app"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
 	m := app.MakeHandler("./test.db")
 
 	// app이 종료되기전에 Close 호출
 	defer m.Close()
 
 	log.Println("Started App")
-	err := http.ListenAndServe(":3000", m)
+	err := http.ListenAndServe(":port", m)
 	if err != nil {
 		panic(err)
 	}
